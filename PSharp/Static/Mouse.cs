f3436e36897c1fd436a1342803ddb11c2a39e90f
Luis Alberto Flores Baca <betofloresbaca@gmail.com>
2024-02-04 19:07:48 -0600

    Add MouseButton property to Mouse static class

--- PSharp/Static/Mouse.cs
+++ PSharp/Static/Mouse.cs
@@ -12,6 +12,37 @@ namespace PSharp.Static
 
         public static int MouseY => Raylib.GetMouseY();
 
-        public static bool MouseIsPressed => Raylib.IsMouseButtonDown(MouseButton.Left);
+        public static bool MousePressed => MouseButton is not null;
+
+        public enum MouseButtonEnum
+        {
+            LEFT,
+            RIGHT,
+            CENTER
+        }
+
+        public static MouseButtonEnum LEFT = MouseButtonEnum.LEFT;
+        public static MouseButtonEnum RIGHT = MouseButtonEnum.RIGHT;
+        public static MouseButtonEnum CENTER = MouseButtonEnum.CENTER;
+
+        public static MouseButtonEnum? MouseButton
+        {
+            get
+            {
+                if (Raylib.IsMouseButtonPressed(Raylib_cs.MouseButton.Left))
+                {
+                    return LEFT;
+                }
+                if (Raylib.IsMouseButtonPressed(Raylib_cs.MouseButton.Right))
+                {
+                    return RIGHT;
+                }
+                if (Raylib.IsMouseButtonPressed(Raylib_cs.MouseButton.Middle))
+                {
+                    return CENTER;
+                }
+                return null;
+            }
+        }
     }
 }
