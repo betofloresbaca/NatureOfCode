1afee61e0410e556e89b8e19891a5e0f4a84179a
Luis Alberto Flores Baca <betofloresbaca@gmail.com>
2024-02-04 18:57:46 -0600

    Add string functions

---
+++ PSharp/Static/String.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace PSharp.Static
+{
+    public static class String
+    {
+        public static string Join(string[] strings, string separator) =>
+            string.Join(separator, strings);
+
+        public static string Nfc(int num, int right = 2) => num.ToString("N" + right);
+
+        public static string[] Nfc(int[] nums, int right = 2) =>
+            nums.Select(x => Nfc(x, right)).ToArray();
+
+        public static string Nfc(double num, int right = 2) => num.ToString("N" + right);
+
+        public static string[] Nfc(double[] nums, int right = 2) =>
+            nums.Select(x => Nfc(x, right)).ToArray();
+
+        public static string Trim(string str) => str.Trim();
+    }
+}
